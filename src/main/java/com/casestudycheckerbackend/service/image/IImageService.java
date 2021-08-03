fdaf04ffdc98b764bd4996e46c55e3e5c13de94b
congle98 <79907265+congle98@users.noreply.github.com>
2021-08-03 16:57:19 +0700

    fix-service-user

--- src/main/java/com/casestudycheckerbackend/service/image/IImageService.java
+++ src/main/java/com/casestudycheckerbackend/service/image/IImageService.java
@@ -1,5 +1,6 @@
 package com.casestudycheckerbackend.service.image;
 
+import com.casestudycheckerbackend.dto.request.UpdateAvatarRequest;
 import com.casestudycheckerbackend.models.Image;
 import com.casestudycheckerbackend.models.User;
 import com.casestudycheckerbackend.service.IGeneralService;
@@ -7,4 +8,6 @@ import com.casestudycheckerbackend.service.IGeneralService;
 import java.util.List;
 
 public interface IImageService extends IGeneralService<Image> {
+    Image updateAvatarOfUserInformation(UpdateAvatarRequest updateAvatarRequest);
+    Image addImage(UpdateAvatarRequest updateAvatarRequest);
 }
--- src/main/java/com/casestudycheckerbackend/service/servicesProvided/ServicesProvidedService.java
+++ src/main/java/com/casestudycheckerbackend/service/servicesProvided/ServicesProvidedService.java
@@ -1,4 +1,35 @@
 package com.casestudycheckerbackend.service.servicesProvided;
 
-public class ServicesProvidedService {
+import com.casestudycheckerbackend.models.ServicesProvided;
+import com.casestudycheckerbackend.repository.ServicesProviderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ServicesProvidedService implements IServicesProvidedService{
+    @Autowired
+    private ServicesProviderRepository servicesRepository;
+
+
+    @Override
+    public Iterable<ServicesProvided> findAll() {
+        return servicesRepository.findAll();
+    }
+
+    @Override
+    public Optional<ServicesProvided> findById(Long id) {
+        return servicesRepository.findById(id);
+    }
+
+    @Override
+    public void delete(Long id) {
+            servicesRepository.deleteById(id);
+    }
+
+    @Override
+    public ServicesProvided save(ServicesProvided servicesProvided) {
+        return servicesRepository.save(servicesProvided);
+    }
 }
