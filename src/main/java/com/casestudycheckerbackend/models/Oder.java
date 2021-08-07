6d3f28dfdb3cb2c5133d3e4c680bc3e2ee54e9d1
hoang92-gtvt <hoang92.gtvt@gmail.com>
2021-08-07 22:49:03 +0700

    xu ly edit order

--- src/main/java/com/casestudycheckerbackend/models/Oder.java
+++ src/main/java/com/casestudycheckerbackend/models/Oder.java
@@ -35,4 +35,84 @@ public class Oder {
     @OneToOne FeedbackOrder feedback;
 
 
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public UserInformation getUser() {
+        return user;
+    }
+
+    public void setUser(UserInformation user) {
+        this.user = user;
+    }
+
+    public UserInformation getProvider() {
+        return provider;
+    }
+
+    public void setProvider(UserInformation provider) {
+        this.provider = provider;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Long getHour() {
+        return hour;
+    }
+
+    public void setHour(Long hour) {
+        this.hour = hour;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalDate getDay() {
+        return day;
+    }
+
+    public void setDay(LocalDate day) {
+        this.day = day;
+    }
+
+    public Double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(Double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public StatusOder getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusOder status) {
+        this.status = status;
+    }
+
+    public FeedbackOrder getFeedback() {
+        return feedback;
+    }
+
+    public void setFeedback(FeedbackOrder feedback) {
+        this.feedback = feedback;
+    }
 }
